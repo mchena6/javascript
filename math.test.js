62dd063bd2bae6f51b4e05e2276540adbf95fcfe
mchena <m.chena@itecriocuarto.org.ar>
2025-08-18 22:05:37 -0300

    multiplicacion y division agregado al test

--- math.test.js
+++ math.test.js
@@ -1,3 +1,6 @@
-const { suma, resta } = require('./math');
+const { suma, resta, multiplicacion, division } = require('./math');
 
-console.assert(suma(2, 3) === 5) 
+console.assert(suma(2, 3) === 5);
+console.assert(resta(2,3) === -1);
+console.assert(multiplicacion(2,3) === 6)
+console.assert(division(6,2) === 3)
