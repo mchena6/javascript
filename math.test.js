6581d932fc0d5f080f415ef85c9338ef40929dbe
mchena <m.chena@itecriocuarto.org.ar>
2025-08-18 22:31:39 -0300

    rompiendo test

--- math.test.js
+++ math.test.js
@@ -1,6 +1,3 @@
 const { suma, resta, multiplicacion, division } = require('./math');
 
-console.assert(suma(2, 3) === 5);
-console.assert(resta(2,3) === -1);
-console.assert(multiplicacion(2,3) === 6)
-console.assert(division(6,2) === 3)
+console.assert(suma(2, 3) === 6);
